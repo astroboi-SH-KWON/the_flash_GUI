dbee7d060c429377107ef65f2b57ee6072b84271
astroboi-lab <terry007x@korea.ac.kr>
2020-11-23 12:49:21 +0900

    ing

--- Test.py
+++ Test.py
@@ -3,13 +3,15 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 import multiprocessing as mp
+import os
+import shutil
 
 ############### start to set env ################
 WIN = None
 INPUT_LIST = []
-BTN_LIST = []
-FILE_LIST = []
+FILE_LIST = None
 FILE_INPUT_LABEL = None
+LOG_TXT = None
 
 TOTAL_CPU = mp.cpu_count()
 MULTI_CNT = int(TOTAL_CPU*0.5)
@@ -22,13 +24,10 @@ def get_opt_list(mult_cnt, tot_cpu):
     else:
         return mult_cnt // 2, [i for i in range(1, mult_cnt + 1)]
 
-# TODO
+
 def upload_file(trgt_ent):
-    print(trgt_ent)
     trgt_ent.delete(0, 'end')
     trgt_ent.insert(0, filedialog.askopenfilename())
-
-
 
 
 def get_cpu_num(cpu_num):
@@ -59,36 +58,145 @@ def get_cpu_num(cpu_num):
         # en file dialog
 
 
-def run_flash():
+def valid_file_list(fl_list):
+    non_data_flag = True
+    for idx in range(len(fl_list))[::-1]:
+        fl1 = fl_list[idx][::-1][0]
+        fl2 = fl_list[idx][::-1][1]
+        if fl1.replace(' ', '') == '' and fl2.replace(' ', '') == '':
+            continue
+        elif fl1.replace(' ', '') == '':
+            messagebox.showerror('error', 'check row ' + str(idx + 1) + ' and the first file path')
+            return False
+        elif fl2.replace(' ', '') == '':
+            messagebox.showerror('error', 'check row ' + str(idx + 1) + ' and the second file path')
+            return False
+        else:
+            non_data_flag = False
+    if non_data_flag:
+        messagebox.showerror('error', 'check file path')
+        return False
+    return True
+
+
+def make_dir():
+    global LOG_TXT
+    fq = '/FASTQ_File'
+    co_pr = '/co_product_files'
+    try:
+        LOG_TXT.insert(CURRENT, 'make useless file folders\n\t => ' + fq + '\n\t => ' + co_pr + '\n')
+        os.makedirs(fq, exist_ok=True)
+        os.makedirs(co_pr, exist_ok=True)
+    except Exception as err:
+        LOG_TXT.insert(CURRENT, '[ERROR] during making useless file folders\n' + str(err))
+
+
+def excute_flash(fq1, fq2, output_path):
+    global LOG_TXT
+
+    LOG_TXT.insert(CURRENT, 'excute_flash\n')
+    try:
+        # log = os.popen('cmd args').readline()
+        # print(log, "::::::::::::::::::::::::::::::::::::::")
+        os.system('flash {} {} -M 400 -m 10 -O -o {}'.format(fq1, fq2, output_path))
+        LOG_TXT.insert(CURRENT, 'DONE :::::::::::::::::::::: \n\t' + output_path + ':'*20 + '\n')
+        return
+
+    except Exception as err:
+        LOG_TXT.insert(CURRENT, '[ERROR] during flashing with \n\t' + output_path + '\n' + str(err))
+
+
+def rename_FASTQ(o_path):
+    os.rename(o_path + '.extendedFrags.fastq', o_path + '.fastq')
+
+
+def move_useless(output):
+    global LOG_TXT
+
+    LOG_TXT.insert(CURRENT, 'move files to /co_product_files\n\t' + output + '\n')
+    try:
+        file1 = '{}.hist'.format(output)
+        file2 = '{}.hist.innie'.format(output)
+        file3 = '{}.hist.outie'.format(output)
+        file4 = '{}.histogram'.format(output)
+        file5 = '{}.histogram.innie'.format(output)
+        file6 = '{}.histogram.outie'.format(output)
+        file7 = '{}.notCombined_1.fastq'.format(output)
+        file8 = '{}.notCombined_2.fastq'.format(output)
+        file_list = [file1, file2, file3, file4, file5, file6, file7, file8]
+
+        for i in file_list:
+            shutil.move(i, '/co_product_files')
+        LOG_TXT.insert(CURRENT, 'DONE : move files to /co_product_files\n\t' + output + '\n')
+        return
+    except Exception as err:
+        LOG_TXT.insert(CURRENT, '[ERROR] during moving files to /co_product_files \n\t' + output + '\n' + str(err))
+
+
+def move_FASTQ(output):
+    global LOG_TXT
+
+    LOG_TXT.insert(CURRENT, 'move FASTQ\n\t' + output + '\n')
+    try:
+        shutil.move('{}.fastq'.format(output), '/FASTQ_File')
+        LOG_TXT.insert(CURRENT, 'DONE : move FASTQ\n\t' + output + '\n')
+        return
+    except Exception as err:
+        LOG_TXT.insert(CURRENT, '[ERROR] during FASTQ \n\t' + output + '\n' + str(err))
+
+
+def run_flash(fq1, fq2, output_path):
+    global LOG_TXT
+
+    LOG_TXT.insert(CURRENT, 'RUN the FLASH\n')
+    make_dir()
+    excute_flash(fq1, fq2, output_path)
+    rename_FASTQ(output_path)
+    move_useless(output_path)
+    move_FASTQ(output_path)
+    LOG_TXT.insert(CURRENT, 'DONE ::::::::::::::::: \nRUN the FLASH\n')
+    return
+
+
+def click_flash():
     global FILE_INPUT_LABEL
     global FILE_LIST
-
     input_list = FILE_INPUT_LABEL.grid_slaves()
-    tmp_arr = []
-    for idx in range(len(input_list))[::-1]:
-        if idx % 2 != 0:
-            fl_path = input_list[idx].get().replace(' ', '')
-            tmp_arr.append(fl_path)
-            if len(tmp_arr) == 2:
-                FILE_LIST.append(tmp_arr)
-                tmp_arr = []
-    print(FILE_LIST)
+    FILE_LIST = []
+    for tmp_arr in input_list:
+        tmp_ent_arr = []
+        for btn_ent in tmp_arr.grid_slaves():
+            if 'Entry' in str(type(btn_ent)):
+                tmp_ent_arr.append(btn_ent.get())
+                if len(tmp_ent_arr) == 2:
+                    FILE_LIST.append(tmp_ent_arr)
+                    tmp_ent_arr = []
+
+    if valid_file_list(FILE_LIST):
+        for tmp in FILE_LIST[::-1]:
+            if '.gz' in tmp[::-1][0].lower():
+                output_path, f_ex = os.path.splitext(tmp[::-1][0])
+                output_path, f_ex = os.path.splitext(output_path)
+            else:
+                output_path, f_ex = os.path.splitext(tmp[::-1][0])
+            proc = mp.Process(target=run_flash, args=(tmp[::-1][0], tmp[::-1][1], output_path.split('/')[-1]))
+            proc.start()
 
 
 def add_upload_file():
     global FILE_INPUT_LABEL
     global WIN
-    _, n_row = FILE_INPUT_LABEL.grid_size()
 
-    # st file dialog
-    # if n_row < 5:
-    #     input_list = FILE_INPUT_LABEL.grid_slaves()
-    #     for tmp_input in input_list:
-    #         tmp_input.destroy()
-    #     idx = 0
-    #     for tmp_input in input_list[::-1]:
-    #         tmp_input.grid(row=idx, columnspan=3, padx=5, pady=5)
-    #         idx += 1
+    _, n_row = FILE_INPUT_LABEL.grid_size()
+    default_cpu, cpu_list = get_opt_list(MULTI_CNT, TOTAL_CPU)
+
+    if cpu_list[-1] < n_row + 1:
+        messagebox.showerror('warning', 'too many CPU')
+        return
+    elif default_cpu <= n_row + 1:
+        if not messagebox.askyesno('alert', 'Recommend ' + str(default_cpu) + ' CPU.\nYour computer could get slow\n, if you use too many CPU.\nDo you want to use more CPU?'):
+            return
+
     default_label = Label(FILE_INPUT_LABEL, relief=FLAT)
     default_label.grid(row=n_row, columnspan=3, padx=5, pady=5)
     default_ent1 = Entry(default_label, font='Terminal 10', width=50)
@@ -112,17 +220,23 @@ def del_upload_file():
         messagebox.showerror('info', 'You can NOT remove the last row')
         return
     input_list = FILE_INPUT_LABEL.grid_slaves()
-    input_list[0].grid_remove()
+    input_list[0].destroy()
+    # FILE_INPUT_LABEL[0].grid_remove()
 
 
 def reset():
-    print('reset')
+    global FILE_INPUT_LABEL
+    input_list = FILE_INPUT_LABEL.grid_slaves()
+    for tmp_arr in input_list:
+        for btn_ent in tmp_arr.grid_slaves():
+            if 'Entry' in str(type(btn_ent)):
+                btn_ent.delete(0, 'end')
+
 
 def setupGUI():
     global WIN
     global FILE_INPUT_LABEL
-    global INPUT_LIST
-    global BTN_LIST
+    global LOG_TXT
 
     WIN = Tk()
     WIN.title('the FLASH for GUI')
@@ -137,15 +251,14 @@ def setupGUI():
     spec_label.grid(row=0, columnspan=3, padx=3, pady=3)
     # st drop down selection box
     default_cpu, cpu_list = get_opt_list(MULTI_CNT, TOTAL_CPU)
-    variable = StringVar(spec_label)
-    variable.set(default_cpu)
-    opt = OptionMenu(spec_label, variable, *cpu_list, command=get_cpu_num)
-    opt.config(font=('Terminal bold', 11))
-    opt.grid(row=0, column=0, padx=5, pady=3)
-    cpu_label = Label(spec_label, text='CPU', font='Courier 15 bold')
-    cpu_label.grid(row=0, column=1, padx=5, pady=3)
-    Button(spec_label, text='+', font='Courier 15 bold', command=add_upload_file, width=7).grid(row=0, column=2, padx=3, pady=3)
-    Button(spec_label, text='-', font='Courier 15 bold', command=del_upload_file, width=7).grid(row=0, column=3, padx=3, pady=3)
+    recmd_cpu = ' Recommend ' + str(default_cpu) + ' CPU, Max ' + str(cpu_list[-1]) + ' CPU '
+    cpu_label = Label(spec_label, text=recmd_cpu, font='Courier 20 bold', relief=RAISED)
+    cpu_label.grid(row=0, column=0, padx=5, pady=3)
+
+    Button(spec_label, text='+', font='Courier 15 bold', command=add_upload_file, width=5).grid(row=0, column=2, padx=3,
+                                                                                                pady=3)
+    Button(spec_label, text='-', font='Courier 15 bold', command=del_upload_file, width=5).grid(row=0, column=3, padx=3,
+                                                                                                pady=3)
     # en drop down selection box
 
     # st file_input_label
@@ -169,14 +282,14 @@ def setupGUI():
     # en file dialog
 
     # Text for log
-    log_text = Text(frame1, font='Terminal 10', relief=RAISED, width=100, height=20)
-    log_text.grid(row=2, columnspan=1, padx=3, pady=3)
+    LOG_TXT = Text(frame1, font='Terminal 10', relief=RAISED, width=100, height=20)
+    LOG_TXT.grid(row=2, columnspan=1, padx=3, pady=3)
 
     btn_label = Label(frame1, relief=FLAT)
     btn_label.grid(row=2, column=2, padx=3, pady=3)
     reset_btn = Button(btn_label, text='reset', font='Courier 10 bold', fg='red', command=reset, width=10)
     reset_btn.grid(row=0, column=2, padx=3, pady=5)
-    flash_btn = Button(btn_label, text='run\nthe FLASH', font='Courier 15 bold', command=run_flash, width=10, height=8)
+    flash_btn = Button(btn_label, text='run\nthe FLASH', font='Courier 15 bold', command=click_flash, width=10, height=8)
     flash_btn.grid(row=1, column=2, padx=3, pady=5)
 
     frame2 = Frame(WIN)
